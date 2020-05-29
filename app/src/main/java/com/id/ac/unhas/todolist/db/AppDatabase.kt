e4bad7b17daf4bd6f74a9c54139a28b58837f74c
arsyh09 <arsyh3204@gmail.com>
2020-05-29 16:26:23 +0800

    first commit(CRUD Todo)

---
+++ app/src/main/java/com/id/ac/unhas/todolist/db/AppDatabase.kt
@@ -0,0 +1,34 @@
+package com.id.ac.unhas.todolist.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.id.ac.unhas.todolist.todolist.Todolist
+import com.id.ac.unhas.todolist.todolist.TodolistDao
+
+@Database(entities = [Todolist::class], exportSchema = false, version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun todolistDao(): TodolistDao
+
+    companion object {
+
+        private const val DB_NAME = "todolist_DB"
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase? {
+            if (instance == null) {
+                synchronized(AppDatabase::class) {
+                    instance = Room
+                        .databaseBuilder(
+                            context,
+                            AppDatabase::class.java,
+                            DB_NAME
+                        )
+                        .build()
+                }
+            }
+            return instance
+        }
+    }
+}
